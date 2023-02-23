849b1850b0ea36c7fe4e5645bfe56524719ef88e
Shahin Zaman <shahinzaman@MacAir.local>
2023-02-23 21:58:41 +0600

    first commit

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,77 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { Cart, CartItem } from 'src/app/models/cart.models';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html'
+})
+export class CartComponent implements OnInit {
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 3,
+    id: 2,
+  }]};
+
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ];
+
+  constructor(private cartService: CartService, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) => {
+      let stripe = await loadStripe('pk_test_51MbPsEC9UStJ5bC8OSksnta32sRl4QUlRoSMzkHLCGjPbeyJ4SzRkW8LPex8reWhRjiSVZ2QS1qvzmkv5biC47n700OIYJfqVr');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    });
+  }
+
+}
